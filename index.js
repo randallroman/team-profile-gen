ce3d85918fda2ac8b093cc3cce34fc211c57949a
RoseThorn10 <lrroman@comcast.net>
2022-09-24 21:03:44 -0500

    Completed classes in lib/

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 const inquirer = require('inquirer');
 const jest = require('jest');
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
+var idiot = new Manager('fool', -999, 'fake', '0');
+
+idiot.getName();
+idiot.getRole();
+idiot.getOfficeNumber();
+
+var emp = new Employee('foo', 1, 'also fake');
+
+emp.getName();
+emp.getRole();
+// emp.getOfficeNumber();
+
+var chooChoo = new Engineer('Franklin', 9, 'totally fake', 'notagit');
+
+chooChoo.getName();
+chooChoo.getId();
+chooChoo.getEmail();
+chooChoo.getGitHub();
+
+
+var peon = new Intern('Junior', 3.14, 'so so fake', 'NU');
+
+peon.getName();
+peon.getId();
+peon.getEmail();
+peon.getSchool();
